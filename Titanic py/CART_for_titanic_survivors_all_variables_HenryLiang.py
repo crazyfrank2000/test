e9252c48af1fa2887380c67d31e6d4a51cef2945
crazyfrank2000 <wenjun_shi@hotmail.com>
2019-12-24 11:03:56 +0800

    adding the code for titanic

---
+++ Titanic py/CART_for_titanic_survivors_all_variables_HenryLiang.py
@@ -0,0 +1,51 @@
+# Decision Tree Classification for Titanic Survivors
+
+# Importing the libraries
+import numpy as np
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('titanic3.csv')
+X = dataset.iloc[:, [0, 3, 4, 5, 6, 8]].values
+y = dataset.iloc[:, 1].values
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, [2, 5]])
+X[:, [2, 5]] = imputer.transform(X[:, [2, 5]])
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 1] = labelencoder_X.fit_transform(X[:, 1])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Encoding the Dependent Variable
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Fitting classifier to the Training set
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
